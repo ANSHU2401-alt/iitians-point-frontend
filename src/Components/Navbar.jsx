4ba6962197d3746759ac23e2dd77a8d283099948
anshu <anshucengage020306@gmail.com>
2026-08-27 08:57:28 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Navbar = ({streak}) => {
   const searchinput = useRef(null);
   const [dark, setDark] = useState(false);
@@ -9,7 +9,20 @@ const Navbar = ({streak}) => {
     setDark(prev => !prev);
     document.documentElement.classList.toggle("dark");
   };
+const navigate = useNavigate();
+  const handleLogout = async () => {
+  try {
+    await axios.post(
+      "https://iitiansbro-seven.vercel.app/logout",
+      {},
+      { withCredentials: true }
+    );
 
+    navigate("/login");
+  } catch (error) {
+    console.log("Logout failed:", error);
+  }
+};
   return (
     <nav className="w-full bg-slate-700 dark:bg-slate-800 text-white p-2.5 md:px-2 md:py-3 justify-between md:justify-around md:flex items-center sticky top-0 z-50 transition-colors duration-300">
       
@@ -51,7 +64,7 @@ const Navbar = ({streak}) => {
           </button>
 
         </div> */}
-        <div className="flex items-center space-x-2.5">
+        {/* <div className="flex items-center space-x-2.5">
 
   {streak <= 0 ? (
     <button className="px-2 py-1 text-white border border-white rounded-2xl cursor-pointer hover:font-semibold w-[60px]">
@@ -62,6 +75,26 @@ const Navbar = ({streak}) => {
       onClick={() => {
         // logout logic yahan
       }}
+      className="px-2 py-1 text-white border border-white rounded-2xl cursor-pointer hover:font-semibold w-[75px]"
+    >
+      Logout
+    </button>
+  )}
+
+  <button className="px-2 py-1 bg-white text-black rounded-2xl cursor-pointer hover:font-semibold w-[80px]">
+    <Link to="/SignUp">Sign Up</Link>
+  </button>
+
+</div> */}
+        <div className="flex items-center space-x-2.5">
+
+  {streak <= 0 ? (
+    <button className="px-2 py-1 text-white border border-white rounded-2xl cursor-pointer hover:font-semibold w-[60px]">
+      <Link to="/login">Login</Link>
+    </button>
+  ) : (
+    <button
+      onClick={handleLogout}
       className="px-2 py-1 text-white border border-white rounded-2xl cursor-pointer hover:font-semibold w-[75px]"
     >
       Logout
